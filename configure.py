436b99d2ff66351622e2c3f12b19cd0d59608660
store iphone perm <127046937+Piprojec@users.noreply.github.com>
2023-06-08 01:17:25 +0500

    configure.py

--- configure.py
+++ configure.py
@@ -1,6 +1,6 @@
 config = {
 	'name': 'storeappleperm',
 	'token': '5877536086:AAHJB4fEAF8tTD6ZrE0G_o8UjoHUHoPxK9A',
-	'tokenqiwi': 'https://api.telegram.org/',
+	'tokenqiwi': '',
 	'phoneqiwi': ''
 }
